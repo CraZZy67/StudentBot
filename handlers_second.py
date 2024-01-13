6f2a89ff5ce5453acb248ea71db6e093362bdbc1
CraZZy67 <sergei89066026767@gmail.com>
2024-01-13 15:20:07 +0300

    Последние приготовления

--- handlers_second.py
+++ handlers_second.py
@@ -10,44 +10,47 @@ from callback_factory import CallbackStudent
 
 additional_router = Router()
 
+try:
+    @additional_router.callback_query(F.data.in_({"LIST", "DELETE", "BACK"}))
+    async def handl_list(callback: CallbackQuery):
+        if callback.data == "LIST":
+            await callback.message.edit_text(text=formatting_text(gs.sheet_d), reply_markup=kb_for_list(gs.sheet_d, view="list"))
+        elif callback.data == "BACK":
+            await callback.message.edit_text("Выберите действие!", reply_markup=start_k())
+        else:
+            await callback.message.edit_text(text=formatting_text(gs.sheet_d), reply_markup=kb_for_list(gs.sheet_d, view="delete"))
 
-@additional_router.callback_query(F.data.in_({"LIST", "DELETE", "BACK"}))
-async def handl_list(callback: CallbackQuery):
-    if callback.data == "LIST":
-        await callback.message.edit_text(text=formatting_text(gs.sheet_d), reply_markup=kb_for_list(gs.sheet_d, view="list"))
-    elif callback.data == "BACK":
-        await callback.message.edit_text("Выберите действие!", reply_markup=start_k())
-    else:
+
+    @additional_router.callback_query(CallbackStudent.filter(F.type == "list"))
+    async def handl_buttons_l(callback: CallbackQuery, callback_data: CallbackStudent):
+        string_or_finish = add_exam_value(gs.sheet_d[f"{callback_data.order - 1}"][2])
+
+        if string_or_finish != "finish":
+            gs.sheet_d[f"{callback_data.order - 1}"][2] = string_or_finish
+
+            if gs.sheet_d[f"{callback_data.order - 1}"][3] == "Занятий еще не было":
+                gs.sheet_d[f"{callback_data.order - 1}"][3] = f"{datetime.now().date()}, "
+            else:
+                gs.sheet_d[f"{callback_data.order - 1}"][3] += f"{datetime.now().date()}, "
+
+            await callback.message.edit_text(text=formatting_text(gs.sheet_d), reply_markup=kb_for_list(gs.sheet_d, view="list"))
+        else:
+
+            gs.sheet_d[f"{callback_data.order - 1}"][3] += str(datetime.now().date())
+            await callback.message.delete()
+            await callback.message.answer("Курс окончен!")
+            await callback.message.answer(f"Имя: {gs.sheet_d[str(callback_data.order - 1)][0]} | Предмет: {gs.sheet_d[str(callback_data.order - 1)][1]} | "
+                                          f"Уроков: {gs.sheet_d[str(callback_data.order - 1)][2][2]}\nДаты: {gs.sheet_d[str(callback_data.order - 1)][3]}")
+            gs.sheet_d[f"{callback_data.order - 1}"][3] = "Занятий еще не было"
+            gs.sheet_d[f"{callback_data.order - 1}"][2] = f"0/{gs.sheet_d[str(callback_data.order - 1)][2][2]}"
+            await callback.message.answer(text=formatting_text(gs.sheet_d), reply_markup=kb_for_list(gs.sheet_d, view="list"))
+
+
+    @additional_router.callback_query(CallbackStudent.filter(F.type == "delete"))
+    async def handl_buttons_l(callback: CallbackQuery, callback_data: CallbackStudent):
+        gs.sheet_d.pop(f"{callback_data.order - 1}")
+        gs.sheet_d = shuffle(gs.sheet_d)
         await callback.message.edit_text(text=formatting_text(gs.sheet_d), reply_markup=kb_for_list(gs.sheet_d, view="delete"))
 
-
-@additional_router.callback_query(CallbackStudent.filter(F.type == "list"))
-async def handl_buttons_l(callback: CallbackQuery, callback_data: CallbackStudent):
-    string_or_finish = add_exam_value(gs.sheet_d[f"{callback_data.order - 1}"][2])
-
-    if string_or_finish != "finish":
-        gs.sheet_d[f"{callback_data.order - 1}"][2] = string_or_finish
-
-        if gs.sheet_d[f"{callback_data.order - 1}"][3] == "Занятий еще не было":
-            gs.sheet_d[f"{callback_data.order - 1}"][3] = f"{datetime.now().date()}, "
-        else:
-            gs.sheet_d[f"{callback_data.order - 1}"][3] += f"{datetime.now().date()}, "
-
-        await callback.message.edit_text(text=formatting_text(gs.sheet_d), reply_markup=kb_for_list(gs.sheet_d, view="list"))
-    else:
-
-        gs.sheet_d[f"{callback_data.order - 1}"][3] += str(datetime.now().date())
-        await callback.message.delete()
-        await callback.message.answer("Курс окончен!")
-        await callback.message.answer(f"Имя: {gs.sheet_d[str(callback_data.order - 1)][0]} | Предмет: {gs.sheet_d[str(callback_data.order - 1)][1]} | "
-                                      f"Уроков: {gs.sheet_d[str(callback_data.order - 1)][2][2]}\nДаты: {gs.sheet_d[str(callback_data.order - 1)][3]}")
-        gs.sheet_d[f"{callback_data.order - 1}"][3] = "Занятий еще не было"
-        gs.sheet_d[f"{callback_data.order - 1}"][2] = f"0/{gs.sheet_d[str(callback_data.order - 1)][2][2]}"
-        await callback.message.answer(text=formatting_text(gs.sheet_d), reply_markup=kb_for_list(gs.sheet_d, view="list"))
-
-
-@additional_router.callback_query(CallbackStudent.filter(F.type == "delete"))
-async def handl_buttons_l(callback: CallbackQuery, callback_data: CallbackStudent):
-    gs.sheet_d.pop(f"{callback_data.order - 1}")
-    gs.sheet_d = shuffle(gs.sheet_d)
-    await callback.message.edit_text(text=formatting_text(gs.sheet_d), reply_markup=kb_for_list(gs.sheet_d, view="delete"))
+except Exception as ex:
+    print(f"Возникла ошибка: {ex}, в {__name__}")
